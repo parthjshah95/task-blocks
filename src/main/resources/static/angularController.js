d4f0c378548d5cfe3e143fce266df223aa360d5d
xorcist-parth <parthjshah95@gmail.com>
2016-10-11 20:48:32 +0530

    added float for story points selection

--- src/main/resources/static/angularController.js
+++ src/main/resources/static/angularController.js
@@ -3,6 +3,8 @@ angular.module("TF", []).controller('myCtrl', function($scope) {
     $scope.taskTitle = "Task Title";
     $scope.taskTypes = ["Objective","Epic","Story","Sub-task","Float","Sub-float"];
     $scope.taskType = $scope.taskTypes[2];
+    var fibionacciSeries = [1,2,3,5,8,13];
+    $scope.storyPoints = fibionacciSeries[2];
 
 
 
