422e3fd17737f2cfbb825d286a027ca009030470
xorcist-parth <parthjshah95@gmail.com>
2016-10-11 22:56:16 +0530

    added functionality to story points selection; changed color scheme

--- src/main/resources/static/angularController.js
+++ src/main/resources/static/angularController.js
@@ -3,10 +3,23 @@ angular.module("TF", []).controller('myCtrl', function($scope) {
     $scope.taskTitle = "Task Title";
     $scope.taskTypes = ["Objective","Epic","Story","Sub-task","Float","Sub-float"];
     $scope.taskType = $scope.taskTypes[2];
-    var fibionacciSeries = [1,2,3,5,8,13];
-    $scope.storyPoints = fibionacciSeries[2];
+    var i = 2;
+    $scope.fibionacciSeries = [1,2,3,5,8,13];
+    $scope.storyPoints = $scope.fibionacciSeries[i];
 
+    $scope.incrementStoryPoints = function(){
+        if(i <= 4){
+            i = i + 1;
+        }
+        $scope.storyPoints = $scope.fibionacciSeries[i];
+    }
 
+    $scope.decrementStoryPoints = function(){
+        if(i >= 1){
+            i= i - 1;
+        }
+        $scope.storyPoints = $scope.fibionacciSeries[i];
+    }
 
 
 
