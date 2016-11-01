d055f0466af5f6ab619ff184f2a298b839c8e7aa
HERE\parshah <parth.shah@here.com>
2016-11-01 13:25:22 +0530

    enabled web hosting through springboot.

---
+++ src/main/java/com/parth/TaskFunnel/IndexController.java
@@ -0,0 +1,35 @@
+package com.parth.TaskFunnel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*** Created by parshah on 17-Sep-16.*/
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String indexController() {
+
+    return "/index";
+    }
+
+}
