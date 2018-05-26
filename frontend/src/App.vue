a367d2a9ed68308a456bc944e730bb3779999e61
Parth Shah <parthjshah95@gmail.com>
2018-05-27 00:14:21 +0530

    created 2 modules - frontend and backend. Will remove existing frontend once the new frontend module is stable.

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-container fluid id="app">
+    <Header></Header>
+    <Container>
+      <Draggable>foo</Draggable>
+    </Container>
+  </b-container>
+  
+</template>
+
+<script>
+import { Container, Draggable } from "vue-smooth-dnd";
+import Header from "./components/header.vue";
+
+export default {
+  name: 'app',
+  components: { Container, Draggable, Header},
+  methods: {
+    onDrop: function(dropResult) {
+      this.items = applyDrag(this.items, dropResult);
+    }
+  },
+  data: function() {
+    return {
+      msg: 'Dhwan is dhabbooram'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body{
+  margin: 0;
+}
+#app{
+  padding:0px;
+}
+</style>
